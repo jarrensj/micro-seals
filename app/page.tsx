dbfa630d0d8c4d914124c34be8deb69d4ad7d252
Jarren San Jose <sanjose.jarren@gmail.com>
2024-03-25 17:15:40 -0700

    add Wagmi, ConnectKit, and a connect wallet button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import ConnectWallet from "@/components/ConnectWallet";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-4xl font-bold">sup</h1>
+      <ConnectWallet />
     </main>
   );
 }
