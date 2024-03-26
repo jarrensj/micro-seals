0a46ee6819c6569eb9da734512601d11de26ac27
Jarren San Jose <sanjose.jarren@gmail.com>
2024-03-25 18:22:41 -0700

    micro sappys

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import MintForm from "@/components/MintForm"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold">sup</h1>
+      <h1 className="text-4xl font-bold">micro sappys</h1>
       <ConnectWallet />
       <MintForm />
     </main>
